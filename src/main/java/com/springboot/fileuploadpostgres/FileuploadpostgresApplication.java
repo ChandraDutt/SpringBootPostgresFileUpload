27597fc07451e0eb8522fd187220c4fc4c5175c1
Chandra Dutt <chandradutt.chandu@gmail.com>
2021-04-15 23:36:09 +0530

    Initial commit

---
+++ src/main/java/com/springboot/fileuploadpostgres/FileuploadpostgresApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.fileuploadpostgres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileuploadpostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileuploadpostgresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/fileuploadpostgres/FileuploadpostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.fileuploadpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileuploadpostgresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
